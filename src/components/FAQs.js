4f6e3ba07336812c9cbb9f47be9231e8b92ddda5
sandeep1995 <i.am.sandeep.acharya@gmail.com>
2023-10-05 17:53:03 +0530

    auto commit

---
+++ src/components/FAQs.js
@@ -0,0 +1,30 @@
+const faqs = [
+  {
+    question: 'What is Netflix?',
+    answer:
+      'Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.',
+  },
+  {
+    question: 'Can I cancel my subscription?',
+    answer:
+      'Of course! You can cancel your plan at any time. There are no cancellation fees, though no refunds are provided for prorated periods.',
+  },
+  {
+    question: 'What can I watch on Netflix?',
+    answer:
+      'Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, any time you want.',
+  },
+  {
+    question: 'Is Netflix good for kids?',
+    answer:
+      'The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and movies in their own space.',
+  },
+];
+
+export default function FAQs() {
+  return (
+    <div className='bg-gray-50'>
+      <div className='max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8'></div>
+    </div>
+  );
+}
